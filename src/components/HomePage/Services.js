06b62e6200d4720e5053c56cacbcb4b0a3cb19a8
nathanganesh <nathangemieee@gmail.com>
2020-08-30 18:49:37 +0200

    services working

--- src/components/HomePage/Services.js
+++ src/components/HomePage/Services.js
@@ -1,5 +1,56 @@
 import React from 'react';
+import { FaDolly, FaRedo, FaDollarSign } from 'react-icons/fa';
+import styled from 'styled-components';
 
 export default function Services() {
-	return <div>Services</div>;
+	let services = [
+		{
+			id: 1,
+			icon: <FaDolly />,
+			title: 'free shipping',
+			text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, accusamus.'
+		},
+		{
+			id: 2,
+			icon: <FaRedo />,
+			title: '30 days return policy',
+			text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, accusamus.'
+		},
+		{
+			id: 3,
+			icon: <FaDollarSign />,
+			title: 'secured payment',
+			text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, accusamus.'
+		}
+	];
+
+	return (
+		<ServicesWrapper>
+			<div className="services">
+				{services.map((item) => {
+					return (
+						<div key={item.id}>
+							<div className="service-icon">{item.icon}</div>
+							<h6>{item.title}</h6>
+							<p>{item.text}</p>
+						</div>
+					);
+				})}
+			</div>
+		</ServicesWrapper>
+	);
 }
+
+const ServicesWrapper = styled.section`
+	.service-icon {
+		font-size: 2.5rem;
+		/* margin-top: 30px; */
+		color: black;
+	}
+	h6 {
+		font-size: 1.2rem;
+	}
+	p {
+		color: black;
+	}
+`;
