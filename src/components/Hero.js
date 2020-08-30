3e2df456ccaa6ec7763255cca3cc07d98ea1e9dc
nathanganesh <nathangemieee@gmail.com>
2020-08-30 17:54:55 +0200

    hero wroking

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Hero() {
-	return <div>hello from Hero</div>;
+export default function Hero({ img, title, max, children }) {
+	return (
+		<HeroWrapper max={max} img={img}>
+			<div className="banner">
+				<h1 className="title">{title}</h1>
+				{children}
+			</div>
+		</HeroWrapper>
+	);
 }
+
+const HeroWrapper = styled.div`
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: ${(props) => (props.max ? '100vh' : '60vh')};
+	color: white;
+	background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+		url(${(props) => props.img}) center/cover no-repeat;
+	.title {
+		padding-top: 2rem;
+		font-size: 3.5rem;
+		text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.3);
+		text-transform: uppercase;
+		letter-spacing: 0.3rem;
+	}
+`;
+
+Hero.defaultProps = {
+	img: window.location.origin + '/static_images/mainBcg.jpeg'
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/HomePage/Services';
 import Featured from '../components/HomePage/Featured';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 	return (
 		<div>
-			<Hero />
+			<Hero max={true} title="Awesome gadgets">
+				<Link to="/product" className="main-link" style={{ margin: '2rem' }}>
+					our products
+				</Link>
+			</Hero>
 			<Services />
 			<Featured />
 		</div>
